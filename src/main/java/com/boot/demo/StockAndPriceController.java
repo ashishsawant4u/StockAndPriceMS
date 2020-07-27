a2fdc532503ddc1511504a5034def26e458f1f1d
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-28 01:18:30 +0530

    Using Cloud Sleuth

--- src/main/java/com/boot/demo/StockAndPriceController.java
+++ src/main/java/com/boot/demo/StockAndPriceController.java
@@ -1,5 +1,9 @@
 package com.boot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,25 +15,34 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class StockAndPriceController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(StockAndPriceController.class);
+	
+	@Bean
+	public RestTemplate template() {
+	    return new RestTemplate();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@RequestMapping(path = "/getStockAndPrice",method = RequestMethod.POST)
 	public @ResponseBody StockAndPriceData getStockAndPriceForProd(@RequestBody String productCode)
 	{	
-		System.out.println("Fetching Stock for "+productCode);
+		LOG.info("inside getStockAndPriceForProd...");
+		
+		LOG.info("Fetching Stock for "+productCode);
 		
 		StockAndPriceData data = new StockAndPriceData();
 		
 		data.setOfflineStock(1200);
 		data.setOnlineStock(700);
 		
-		RestTemplate restTemplate = new RestTemplate();
 		
-		
-		String stockAndPriceUrl = "http://localhost:8024/getPrice";
+		String stockAndPriceUrl = "http://localhost:8024/pricingMS/getPrice";
 		
 		ResponseEntity<PriceData> resp = restTemplate.postForEntity(stockAndPriceUrl, productCode,PriceData.class);
 		
-		System.out.println("resp from price MS  >> "+resp.getBody().getUnitPrice());
+		LOG.info("resp from price MS  >> "+resp.getBody().getUnitPrice());
 		
 		data.setUnitPrice(resp.getBody().getUnitPrice());
 		data.setUom(resp.getBody().getUom());
